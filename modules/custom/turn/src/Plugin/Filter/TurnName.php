94a676784a159b8f441f631118cc7f9ce6c2966d
hayodehaai <50200969+hayodehaai@users.noreply.github.com>
2019-12-13 16:03:00 +0100

    Update TurnName.php

--- modules/custom/turn/src/Plugin/Filter/TurnName.php
+++ modules/custom/turn/src/Plugin/Filter/TurnName.php
@@ -6,12 +6,10 @@ use Drupal\filter\FilterProcessResult;
 use Drupal\filter\Plugin\FilterBase;
 
 /**
- * @file
- * Contains Drupal\turn\Plugin\Filter\TurnName.
- */
-
-/**
  * A filter to turn the last- and first name.
+ *
+ * This filter changes the token "[name:FIRSTNAME:LASTNAME]".
+ * To "Name: LASTNAME FIRSTNAME".
  */
 
 /**
@@ -20,19 +18,26 @@ use Drupal\filter\Plugin\FilterBase;
  * @Filter(
  *   id = "filter_turn",
  *   title = @Translation("Turn Name"),
- *   description = @Translation("Turn first- and last name!"),
- *   type = Drupal\filter\Plugin\FilterInterface::TYPE_MARKUP_LANGUAGE,
+ *   description = @Translation("Turn first- and last name in token [name:FIRSTNAME:LASTNAME]"),
+ *   type = Drupal\filter\Plugin\FilterInterface::TYPE_TRANSFORM_IRREVERSIBLE,
  * )
  */
 class TurnName extends FilterBase {
 
   /**
-   * Filter function.
+   * Filter function to replace the token.
    */
   public function process($text, $langcode) {
-    $replace = $this->t('Name: LASTNAME FIRSTNAME');
-    $new_text = str_replace('[name:FIRSTNAME:LASTNAME]', $replace, $text);
-    $result = new FilterProcessResult($new_text);
+    // Pattern to search for: [name:anything:anything].
+    $pattern = '/\[name:(?:.*):(?:.*)\]/';
+    // Scan for token.
+    preg_match($pattern, $text, $match);
+    // Store result in array.
+    $name = explode(":", (str_replace(["[name:", "]"], "", $match[0])));
+    $replace = $this->t("Name: @lastname @firstname", ["@firstname" => $name[0], "@lastname" => $name[1]]);
+    // Replace token.
+    $token_replaced = preg_replace($pattern, $replace, $text);
+    $result = new FilterProcessResult($token_replaced);
     return $result;
   }
 
